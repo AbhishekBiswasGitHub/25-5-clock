0da85bd871bc181a06c94d693512ff8de58d8e32
abbup <abhishek.biswas.git@gmail.com>
2023-08-09 10:53:46 +0530

    Add component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useMemo, useState } from "react";
+
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  const [active, setActive] = useState(false);
+  const [sessionTime, setSessionTime] =
+    useState(25);
+  const [breakTime, setBreakTime] = useState(5);
+
+  const reset = () => {
+    setActive(() => false);
+    setSessionTime(() => 25);
+    setBreakTime(() => 5);
+  };
+
+  const settings = useMemo(
+    () => [
+      {
+        name: "Session",
+        value: sessionTime,
+        handler: setSessionTime,
+      },
+      {
+        name: "Break",
+        value: breakTime,
+        handler: setBreakTime,
+      },
+    ],
+    [sessionTime, breakTime]
+  );
+
+  return <div id="container"></div>;
 };
 
 export default App;
